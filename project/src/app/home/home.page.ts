fe0bcecc7f3d58714a41982571c9911544312e83
Jakub Vana <j_vana@utb.cz>
2023-11-20 11:15:56 +0100

    init but better

---
+++ project/src/app/home/home.page.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { WeatherModalComponent } from './weather-modal/weather-modal.component';
+import { PlacesService } from '../services/places/places.service';
+//import { Preferences } from '@capacitor/preferences';
+
+// npm install @capacitor/preferences in the app folder
+import { Preferences } from '@capacitor/preferences';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  countriesDataArray: any[] = [];
+
+  constructor(
+    public modalCTRL: ModalController, 
+    public http: HttpClient, 
+    // new
+
+    private placesService: PlacesService
+   // private placesService: PlacesService
+    ) {
+
+    // Step 1 for countries fetch in HomePage constructor
+    const getCountryData = async () => {
+      const { value } = await Preferences.get({
+        key: "homeFetchData"
+      });
+
+      if (value) {
+        const fetchedItems = JSON.parse(value); // T F T F
+        this.countriesDataArray = fetchedItems;
+      }
+    }
+
+    getCountryData();
+
+    }
+
+  async openModal(){
+    const modal = await this.modalCTRL.create({
+      component: WeatherModalComponent
+    });
+
+    await modal.present();
+    const {data: newData, role} = await modal.onWillDismiss();
+
+    // Read data
+    if (role === "location"){
+      console.log(newData);
+
+      this.fetchData(newData);
+
+    }
+  }
+
+
+  sendData(data: any){
+    this.placesService.data = data;
+  }
+
+  fetchData(countries: any){ 
+    this.countriesDataArray = [];
+
+    for (var c of countries){
+      const url = `https://api.datamuse.com/words?rel_jjb=${c.label}`;
+    
+      const subscribeFunction = (label: string) => (data: any) => {
+        var formatedData = {
+          query: label,
+          result: data,
+        }
+        console.log(formatedData);
+        this.countriesDataArray.push(formatedData);
+    
+        // Step 3 save data
+        const saveStoredItems = async () => {
+          await Preferences.set({
+            key: "homeFetchData",
+            value: JSON.stringify(this.countriesDataArray)
+          });
+        };
+    
+        saveStoredItems();
+        console.warn(this.countriesDataArray);
+      };
+    
+      this.http.get(url).subscribe(subscribeFunction(c.label));
+    }
+    console.log("Load data from API");
+    
+  }
+
+}
---
+++ project/src/app/home/weather-modal/weather-modal.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+// Capacitor step 1
+import { Preferences } from '@capacitor/preferences';
+
+@Component({
+  selector: 'app-weather-modal',
+  templateUrl: './weather-modal.component.html',
+  styleUrls: ['./weather-modal.component.scss'],
+})
+export class WeatherModalComponent {
+  items = this.getItems();
+  
+  constructor(private modalCTRL: ModalController) {
+    this.items = [
+      { label: "ocean", checked: false},
+      { label: "duck", checked: false},
+      { label: "silly", checked: false},
+      { label: "dungeon", checked: false},
+      { label: "dragons", checked: false},
+    ];
+
+// Capacitor step 2
+    const getStoredItems = async () => {
+      const { value } = await Preferences.get({
+        key: "jsonData"
+      });
+
+      if (value){
+      const selectedItems = JSON.parse(value); // T F T F
+      // items = [F F F F]
+      this.items.forEach(item => {
+        item.checked = selectedItems.some((selectedItem: { label: string; }) => selectedItem.label ===  item.label);
+      });
+    }
+
+      console.log(value);
+    };
+
+    getStoredItems();
+
+   }
+
+
+   dismissModal(){
+    this.modalCTRL.dismiss(null, "cancel");
+   }
+
+   submit(){
+    const selectedItems = this.items.filter((item) => item.checked);
+
+    // Capacitor step 3
+    const saveStoredItems = async () => {
+      await Preferences.set({
+        key: "jsonData",
+        value: JSON.stringify(selectedItems)
+      });
+    };
+
+    saveStoredItems();
+    this.modalCTRL.dismiss(selectedItems, "location");
+   }
+
+
+   getItems(){
+    return [
+      { label: "ocean", checked: false},
+      { label: "duck", checked: false},
+      { label: "silly", checked: false},
+      { label: "dungeon", checked: false},
+      { label: "dragons", checked: false},
+    ]
+   }
+
+
+
+}
